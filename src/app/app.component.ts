3029bedf08653e6b4aeadfe2943a4d098646771a
fadi quader <fadi.quader@gmail.com>
2018-08-08 23:34:13 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,10 @@ import {LogService} from './services/log.service';
 })
 export class AppComponent {
   title = 'app';
-  constructor(logService: LogService){ }
+  constructor(logService: LogService) {
+    // Create an _injector_ and ask for it to resolve and create a LogService
+    // const injector: any = Injector.create([{provide: LogService, useClass: LogService}]);
+    // use the injector to **get the instance** of the LogService
+    // this.logService = injector.get(LogService);
+  }
 }
