fc1f78400bdcb7e64b3022fc2006361ffaeaca62
fadi quader <fadi.quader@gmail.com>
2018-08-08 23:22:13 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
+import {FormsModule} from '@angular/forms';
+import {LogService} from './services/log.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: 'API_URL', useValue: 'http://my.api.com/v1' },
+    {
+      provide: LogService,
+      useFactory() {
+        const currentDate = new Date();
+        return new LogService(currentDate);
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
